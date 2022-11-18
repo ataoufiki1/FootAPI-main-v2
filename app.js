9710d1ccb1f1bee27abb31c752ad65376f55d982
mohaelyas2020 <mohaelyas11@gmail.com>
2022-11-18 12:21:50 +0100

    Final Version

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
-    let options = {
+    const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': 'ca0d916c0dmsh2c7e31c8d06c774p11d0e2jsn7e760bf3bc32',
+            'X-RapidAPI-Key':'ca0d916c0dmsh2c7e31c8d06c774p11d0e2jsn7e760bf3bc32' ,
             'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
         }
     }
@@ -17,13 +17,11 @@ fetch(`https://api-football-v1.p.rapidapi.com/v3/teams?league=39&season=${annee}
     console.log(data.response);
     data.response.forEach(element => {
                 let select= `<select>`  
-                id = document.getElementById("contr").value; 
+                id = document.getElementById("select-team").value; 
                 
-                document.getElementById("contr").innerHTML +=` <option value="${element.team.id}"> ${element.team.name} </option>`;
+                document.getElementById("select-team").innerHTML +=` <option value="${element.team.id}"> ${element.team.name} </option>`;
                 document.getElementById("logo").logo +=` <option value="${element.team.id}"> ${element.team.name} </option>`;
-                // document.getElementById("id").innerHTML +=` <option> ${element.team.id} </option>`;
                       select = `</select>` 
-                     // myFunction()
                      
                   
       })
@@ -31,10 +29,10 @@ fetch(`https://api-football-v1.p.rapidapi.com/v3/teams?league=39&season=${annee}
 
 
 function myFunction() {
-    id = document.getElementById("contr").value; 
+    id = document.getElementById("select-team").value; 
     console.log(`https://api-football-v1.p.rapidapi.com/v3/players?team=${id}&season=${annee}`);
  
-document.getElementById("demo").innerHTML = "You selected: " + id;
+document.getElementById("team-id").innerHTML = "Vous avez choisi:  " + id;
 
 // apres un changement dans le select d'equipe, je recupere l'id de l'equipe puis je crée le <select> des joueurs
 fetch(`https://api-football-v1.p.rapidapi.com/v3/players?team=${id}&season=${annee}`, options)//39
@@ -46,7 +44,7 @@ fetch(`https://api-football-v1.p.rapidapi.com/v3/players?team=${id}&season=${ann
 
                 let select= `<select>`
              
-                      document.getElementById("cont").innerHTML += `<option> ${element.player.name} </option>`;
+                      document.getElementById("select-player").innerHTML += `<option> ${element.player.name} </option>`;
                       select = `</select>`
 
                       
@@ -58,19 +56,9 @@ fetch(`https://api-football-v1.p.rapidapi.com/v3/players?team=${id}&season=${ann
 
 
 
+const btn = document.querySelector("#btn");
 
-
-
-
-
-
-
-
-
-
-const btnn =document.querySelector("#btnn");
-
-btnn.addEventListener("click",() => {
+   btn.addEventListener("click", () => {
 
      
 
@@ -79,133 +67,53 @@ btnn.addEventListener("click",() => {
     
         .then(response => response.json())
         .then((data) => {
-    
-    
 
-    
-            //--------
-          
 
-            //--------
+           
             let affichage =``
             let age =``
             let im =``
+            let logo =``
+            let nationality =``
+            let club =``
+            let weight =``
+            let height =``
+
     for( let x of data.response)
     {
-        let input = document.querySelector("#cont");
+        let input = document.querySelector("#select-player");
          if(input.value === x.player.name)
          {
+
+
             affichage = `<strong>${x.player.name}</strong>`
-            age = `<strong>${x.player.age}</strong>`
+            age = `<strong>${x.player.age} ans</strong>`
+            nationality = `<strong>${x.player.nationality}</strong>`
+            weight = `<strong>${x.player.weight}</strong>`
+            height = `<strong>${x.player.height}</strong>`
             im = x.player.photo
+
          }
-      
+
+
+         else(input.value === x.statistics.team) 
+         {
+            logo = x.statistics[0].team.logo
+            club = `<strong>${x.statistics[0].team.name}</strong>`
+
+        }
+
     }
     affichage +=`</ul>`
     document.querySelector("#namme").innerHTML = affichage;
     document.querySelector("#age").innerHTML = age;
     document.querySelector("#img").src = im;
-    
-    
-    
-    
+    document.querySelector("#logo").src = logo;
+    document.querySelector('#nationality').innerHTML = nationality;
+    document.querySelector('#weight').innerHTML = weight;
+    document.querySelector('#height').innerHTML = height;
+    document.querySelector('#club').innerHTML = club;
+
         })
-
-//--------------
-
-
-
     })
    
-
-
-  
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    // function getData(data) {
-    
-// }
-
-//         //----------------
-        // function getData(data) {
-
-        //     console.log(data);
-        //     const container3 = document.querySelector("#container3");
-        //     const x = "";
-        //     data.forEach(element => {
-        //         var name = element.player.name;
-               
-            
-                  
-        //             document.getElementById("name").innerHTML += "<div>" + name + "</div>"
-                
-                
-               
-        //     });
-        // }
-
-
-
-
-
-
-
-        //----------- liste des joueurs
-
-// let date = new Date()
-// let annee = date.getFullYear();
-// let team = id;
-// fetch(`https://api-football-v1.p.rapidapi.com/v3/players?team=${team}&season=${annee}`, options)
-
-// .then(response => response.json())
-// .then((data) => {
-
-//     data.response.forEach(element => {
-
-       
-      
-//                 let select= `<select>`
-             
-                      
-//                       document.getElementById("cont").innerHTML += `<option> ${element.player.name} </option>`;
-                      
-             
-                   
-                     
-                
-      
-                    
-      
-      
-//                       select = `</select>`
-
-                      
-//       })
-      
-// })
-
-
-//--
